a49329d3654b8c9db378a7a12b6018e70f8eee45
Axel Conceicao <axel.conceicao@epitech.eu>
2019-05-29 05:46:39 +0000

    renderstatus irrgraphical

--- include/Interfaces/Irrlicht/IrrGraphical.hpp
+++ include/Interfaces/Irrlicht/IrrGraphical.hpp
@@ -8,34 +8,41 @@
 #ifndef IRRGRAPHICAL_HPP_
 #define IRRGRAPHICAL_HPP_
 
+static const int GAME = 0;
+static const int MAIN_MENU = 1;
+static const int PAUSE_MENU = 2;
+
 #include "IGraphical.hpp"
 #include "IrrEntity.hpp"
 #include "IrrEvent.hpp"
 #include <irr/irrlicht.h>
 
-namespace IndieStudio {
-	class IrrGraphical : public IGraphical, public irr::IEventReceiver {
-		public:
-			IrrGraphical();
-			~IrrGraphical();
-			virtual IndieStudio::IEntity *createMesh(std::string meshPath) const noexcept;
-			virtual IndieStudio::IEntity *createAnimatedMesh(std::string meshPath, std::string texturePath) const noexcept;
-			virtual IndieStudio::IEntity *createCube(float size, std::string texturePath, IndieStudio::Pos pos, IndieStudio::Pos scale) const noexcept;
-			virtual IndieStudio::IEntity *createParticle(IndieStudio::Pos boxPos, IndieStudio::Pos dirPos, int min, int max, IndieStudio::Pos colorMin, IndieStudio::Pos colorMax) const noexcept;
-			virtual void setCursorVisible(bool b) noexcept;
-			virtual bool run(void) const noexcept;
-			virtual void render(void) const noexcept;
-			virtual void drop(void) const noexcept;
-			virtual bool OnEvent(const irr::SEvent &event);
-			virtual IndieStudio::IEvent getEvent(void) const noexcept;
-			virtual void deleteEntity(IndieStudio::IEntity *) const noexcept;
+namespace IndieStudio
+{
+class IrrGraphical : public IGraphical, public irr::IEventReceiver
+{
+public:
+	IrrGraphical();
+	~IrrGraphical();
+	virtual IndieStudio::IEntity *createMesh(std::string meshPath) const noexcept;
+	virtual IndieStudio::IEntity *createAnimatedMesh(std::string meshPath, std::string texturePath) const noexcept;
+	virtual IndieStudio::IEntity *createCube(float size, std::string texturePath, IndieStudio::Pos pos, IndieStudio::Pos scale) const noexcept;
+	virtual IndieStudio::IEntity *createParticle(IndieStudio::Pos boxPos, IndieStudio::Pos dirPos, int min, int max, IndieStudio::Pos colorMin, IndieStudio::Pos colorMax) const noexcept;
+	virtual void setCursorVisible(bool b) noexcept;
+	virtual bool run(void) const noexcept;
+	virtual void render(void) const noexcept;
+	virtual void drop(void) const noexcept;
+	virtual bool OnEvent(const irr::SEvent &event);
+	virtual IndieStudio::IEvent getEvent(void) const noexcept;
+	virtual void deleteEntity(IndieStudio::IEntity *) const noexcept;
 
-		private:
-			IndieStudio::IrrEvent _event;
-			irr::IrrlichtDevice *_device;
-			irr::scene::ISceneManager *_sceneManager;
-			irr::video::IVideoDriver *_driver;
-	};
+private:
+	int _renderStatus = MAIN_MENU;
+	IndieStudio::IrrEvent _event;
+	irr::IrrlichtDevice *_device;
+	irr::scene::ISceneManager *_sceneManager;
+	irr::video::IVideoDriver *_driver;
 };
+}; // namespace IndieStudio
 
 #endif

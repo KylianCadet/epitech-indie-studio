8130df8605b6dc1e814fdbe89337e4ce6d788f1d
Axel Conceicao <axel.conceicao@epitech.eu>
2019-05-31 04:23:01 +0000

    - Music Manager done

--- src/Menu/MenuSounds.cpp
+++ src/Menu/MenuSounds.cpp
@@ -12,7 +12,6 @@ IndieStudio::MenuSounds::MenuSounds(Volume *volume) : _volume(volume)
 	this->_buttonReturnSound = this->_volume->createSoundEffect("sounds/menu/turnoff.ogg");
 	this->_buttonSwitchSound = this->_volume->createSoundEffect("sounds/menu/tick.ogg");
 	this->_volumeSwitchSound = this->_volume->createSoundEffect("sounds/menu/vol.ogg");
-	this->_mainMusic = this->_volume->createSoundMusic("sounds/menu/Stillness2.ogg", true);
 }
 
 IndieStudio::MenuSounds::~MenuSounds()
